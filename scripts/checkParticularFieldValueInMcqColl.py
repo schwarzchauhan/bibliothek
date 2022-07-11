75b81faf0d137b16c63ea959088ed1a5c47e4ad4
schwarzchauhan <harshchauhan0994@gmail.com>
2022-07-11 19:09:24 +0530

    enchanced register api, mcq script, added nodemailer for sending mail

--- scripts/checkParticularFieldValueInMcqColl.py
+++ scripts/checkParticularFieldValueInMcqColl.py
@@ -6,12 +6,24 @@ bibliothekdb = client["bibliothek"]
 mcqCollecn = bibliothekdb["mcqs"]
 
 
-cursor = mcqCollecn.find({})
+cursor = mcqCollecn.find({}, {'_id': 1, 'quesn':1})
 count = mcqCollecn.count_documents({})
 
 
+# If only one parameter is specified, the type() function returns the type of this object, https://www.w3schools.com/python/ref_func_type.asp
 print(type(cursor), count)
 n = 1
-for document in cursor:
-    print(n, document)
+ctTestQ = 0
+for doc in cursor:
+    # Dictionaries are used to store data values in key:value pairs., https://www.w3schools.com/python/python_dictionaries.asp
+    quesn = doc['quesn']
+    if quesn[0:8] == 'testques':
+        print(n, type(doc), doc['quesn'], doc, quesn[0:8])
+        ctTestQ += 1
     n+=1
+
+print('=======================================================================')
+print('=======================================================================')
+print('=======================================================================')
+print('=======================================================================')
+print('total test quesn added by postman runner', ctTestQ)
