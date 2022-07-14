8f8d32984eb324848b2166e5742bfb6ac9a7528d
schwarzchauhan <harshchauhan0994@gmail.com>
2022-07-14 18:17:29 +0530

    test case added , eg doc for flags collecn

--- scripts/insertUser.py
+++ scripts/insertUser.py
@@ -1,11 +1,12 @@
 # to run --  python insertUser.py
 import pymongo
 
-client = pymongo.MongoClient("mongodb+srv://username:<password>@cluster0.tjdvs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://<username>:<password>@cluster0.tjdvs.mongodb.net/<dbname>?retryWrites=true&w=majority")
 bibliothekdb = client["bibliothek"]
-usersCollecn = bibliothekdb["users"]
+usersCollecn = bibliothekdb["flags"]
 
-doc = {"email":"harsh_11913052@nitkkr.ac.in","password":"1234","name":"harsh chauhan","imgUrl":"https://miro.medium.com/max/875/1*7HZGoVt_I3g_3f-j8uwpSg.jpeg","username":"schwarzengineer"} 
+# doc = {"email":"harsh_11913052@nitkkr.ac.in","password":"1234","name":"harsh chauhan","imgUrl":"https://miro.medium.com/max/875/1*7HZGoVt_I3g_3f-j8uwpSg.jpeg","username":"schwarzengineer"} 
+doc  = {"imgUrl":"https://upload.wikimedia.org/wikipedia/commons/2/21/Flag_of_Colombia.svg","farbe":["geld", "blau", "rot"],"artikel":"das","land":"Kolumbien"}
 
 res = usersCollecn.insert_one(doc)
 print(res)
