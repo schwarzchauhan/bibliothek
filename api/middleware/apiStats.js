fd8327d548c92c78a9b81b7cbed60513dc13c728
schwarzchauhan <harshchauhan0994@gmail.com>
2022-07-21 23:13:54 +0530

    issue resolved api trk 305 http

--- api/middleware/apiStats.js
+++ api/middleware/apiStats.js
@@ -1,6 +1,11 @@
 const {readStats, dumpStats} = require('../service/apiStats');
 
 exports.apiCount = (req, res, next) => {
+    
+    // https://stackoverflow.com/questions/58204192/nodejs-express-catch-the-sent-status-code-in-the-response
+    // to avoid 304 status
+    res.setHeader('Last-Modified', (new Date()).toUTCString());
+
     res.on('finish', () => {
         const stats = readStats()
         const event = `${req.method} ${req.originalUrl} ${res.statusCode}`
